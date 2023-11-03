4c193cbdfc69bc31c84d087fde11876cb9fb5831
Abhi-developerr <147245585+Abhi-developerr@users.noreply.github.com>
2023-11-03 12:58:01 +0530

    Add files via upload

--- Ps5-qno1.py
+++ Ps5-qno1.py
@@ -1,1 +1,14 @@
 # Write a program to create a dictionary of hindi words with values as their english translation . provide user with an option to look it up...... 
+
+words = {
+    'good': 'achaa',
+    'bad': 'kharab',
+    'light': 'chamak',
+    'clock': 'ghadi',
+    'square' : 'varg',
+    'secure' : 'surkshit',
+    'separate': 'alag',
+    'house': 'ghar',
+    'clothes': ' kapde',
+    }
+print(words[input("Enter your word here:  ")])
